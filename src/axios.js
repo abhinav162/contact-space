5ee99dc5778ba9bb0a1e37f5700e08faf2749fbc
Abhinav Aditya <abhinavaditya162@gmail.com>
2023-07-14 14:02:40 +0530

    baseURL update

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = () => {
     const token = localStorage.getItem("token");
 
     return axios.create({
-        baseURL: "http://localhost:3000",
+        baseURL: "https://contact-app-backend-liart.vercel.app",
         headers: {
             "Content-Type": "application/json",
             Authorization: "Bearer " + token,
